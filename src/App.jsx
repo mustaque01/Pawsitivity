a0d8b68360806239e21b25be5c0e5a51253fce63
MD-Mustak <mustakarman560@gmail.com>
2025-07-25 14:26:01 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,3 @@ function App() {
 }
 
 export default App
-export function Logo() {
-  return (
-    <div className="logo-container">
-      <img src={reactLogo} className="logo react" alt="React logo" />
-      <img src={viteLogo} className="logo vite" alt="Vite logo" />
-    </div>
-  )
-}
